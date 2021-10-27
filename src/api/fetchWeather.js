e74f22ff088473868954735ab0f6885e2f1f73a8
Pierre Breny <pierre.breny@gmail.com>
2021-10-27 09:36:07 +0200

    all

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+// dotenv
+dotenv.config();
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = process.env.API_KEY
+
+export const fetchWeather = async (query) => {
+    const {data} = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY
+        }
+    });
+
+    return data;
+
+}
